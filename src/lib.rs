abf3a3d02d56bcc2465afdbc583ea92fed233b00
Seyed Mohammad Amin Sadat <reza13771223@gmail.com>
2024-03-14 20:24:41 +0330

    add universe function and triangular membership function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub mod memberships;
+
+#[allow(unused)]
+use memberships::{Triangle, Universe};
 
 #[cfg(test)]
+#[allow(dead_code, unused)]
 mod tests {
+
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let u1: Universe<50> = Universe::new(0.0, 5.0);
+        let tr1 = Triangle::new(&u1, 1.0, 2.0, 3.0);
+        println!("{:?}",tr1);
     }
 }
---
+++ src/memberships/mod.rs
@@ -0,0 +1,76 @@
+#[derive(Debug)]
+pub struct Universe<const N: usize> {
+    pub data: [f64; N],
+}
+
+#[allow(unused)]
+impl<const N: usize> Universe<N> {
+    pub fn new(start: f64, end: f64) -> Self {
+        let mut data = [0.0;N];
+        let n = N as f64;
+        let dt: f64 = (end - start) / n;
+        for i in 0..N {
+            data[i] = start + (i as f64) * dt;
+        }
+        Self { data }
+    }
+}
+
+impl<const N: usize> IntoIterator for Universe<N> {
+    type Item = f64;
+    type IntoIter = std::array::IntoIter<f64,N>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.into_iter()
+    }
+}
+
+impl<'a, const N: usize> IntoIterator for &'a Universe<N> {
+    type Item = &'a f64;
+    type IntoIter = std::slice::Iter<'a, f64>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.data.iter()
+    }
+}
+
+
+
+/*
+    Triangular membership function
+    please note that a < b < c.
+*/
+
+#[derive(Debug)]
+pub struct Triangle<'a, const N: usize> {
+    universe: &'a Universe<N>,
+    mu: [f64; N],
+}
+
+impl<'a, const N: usize> Triangle<'a, N> {
+    pub fn new(universe: &'a Universe<N>, a: f64, b: f64, c: f64) -> Self {
+        assert!(a < b);
+        assert!(b < c);
+        let mut mu: [f64; N] = [0.0; N];
+        for (i, value) in universe.into_iter().enumerate() {
+            let x = value.clone();
+            let data;
+            if x <= a {
+                data = 0.0;
+            } else if x <= b {
+                data = (x - a) / (b - a);
+            } else if x <= c {
+                data = (c - x) / (c - b);
+            } else {
+                data = 0.0;
+            }
+            mu[i] = data;
+        }
+        Self { universe, mu }
+    }
+}
+impl<'a, const N: usize> IntoIterator for Triangle<'a, N> {
+    type Item = f64;
+    type IntoIter = std::array::IntoIter<f64, N>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.mu.into_iter()
+    }
+}
